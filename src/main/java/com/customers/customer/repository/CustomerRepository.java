f219a4a46a2813785b41f2edcda8512c2191808a
Bárbara Liboni <barbaralibonig@gmail.com>
2022-10-20 02:06:57 -0300

    forgot to create branch

--- src/main/java/com/customers/customer/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.customers.customer.repository;
-
-import com.customers.customer.model.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
--- src/main/resources/db/migration/V1__Create_customer_table.sql
+++
@@ -1,5 +0,0 @@
-create table CUSTOMER (
-    ID long not null,
-    NAME varchar(100) not null,
-    DOCUMENT_NUMBER varchar(11) not null
-);'
